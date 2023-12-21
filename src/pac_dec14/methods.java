15d444c7ce17403dfffec028317c7803562f8abe
ManiRs <rsmani998@gmail.com>
2023-12-21 21:29:15 +0530

    first commit

---
+++ src/pac_dec14/methods.java
@@ -0,0 +1,19 @@
+package pac_dec14;
+
+
+public class methods {
+	
+	public void add (int a,int b)
+	{
+		int c=a+b;
+		System.out.println("The Addition of two numbers is " +c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		methods M=new methods();
+		M.add(7, 8);
+
+	}
+
+}
